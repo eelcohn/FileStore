1ed6eab7d2c35a86f83f23d288d2c9e2b3ccd1ee
eelcohn <eelco@huininga.nl>
2018-06-25 16:16:55 +0200

    Update econet.h

--- src/econet.h
+++ src/econet.h
@@ -33,11 +33,23 @@ namespace econet {
 		unsigned char station;
 	} Station;
 
-	typedef struct {
-		unsigned char status;
-		unsigned char dst_network, dst_station, src_network, src_station, control, port; // Must make this a union with data[] someday
+//	typedef struct {
+//		unsigned char status;
+//		unsigned char dst_network, dst_station, src_network, src_station, control, port; // Must make this a union with data[] someday
+//		unsigned char data[ECONET_MAX_FRAMESIZE];
+//	} Frame;
+
+	union Frame {
 		unsigned char data[ECONET_MAX_FRAMESIZE];
-	} Frame;
+		struct {
+			unsigned char dst_network;
+			unsigned char dst_sation;
+			unsigned char src_network;
+			unsigned char src_station;
+		};
+		unsigned char control;
+		unsigned char port;
+	} frame;
 
 	typedef struct {
 		unsigned long timeout;		// When this session will timeout (in Unix time)
